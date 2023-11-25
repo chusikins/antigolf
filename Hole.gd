1e3e32db6a4209ff91ff07ca6cb3736f8e5e22ba
Alice <alisapetrova275@gmail.com>
2023-11-26 00:15:28 +0300

    v2

---
+++ Hole.gd
@@ -0,0 +1,24 @@
+extends CharacterBody2D
+
+
+@export var acceleration:float = 100
+@export var ball: RigidBody2D
+
+var speed = 100
+
+func _physics_process(delta):
+	speed += delta * acceleration
+	var direction = position.direction_to(ball.position).normalized()
+	velocity = direction * speed
+	move_and_slide()
+	
+
+
+func _on_area_2d_area_entered(area):
+	# print(area.name, " entered hole")
+	pass
+
+
+func _on_area_2d_area_exited(area):
+	# print(area.name, " exited hole")
+	pass
---
+++ choose_level.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_level_1_pressed():
+	get_tree().change_scene_to_file("res://level_1.tscn")
+	
+
+
+func _on_level_2_pressed():
+	get_tree().change_scene_to_file("res://level_2.tscn")
