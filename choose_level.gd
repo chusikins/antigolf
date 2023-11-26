179058cff5360cb4a20de35df14dd6a67bb1f89b
chusikins <chusik7@gmail.com>
2023-11-26 19:28:53 +0300

    fix level select

--- choose_level.gd
+++ choose_level.gd
@@ -18,3 +18,15 @@ func _on_level_1_pressed():
 
 func _on_level_2_pressed():
 	get_tree().change_scene_to_file("res://level_2.tscn")
+
+
+func _on_level_3_pressed():
+	get_tree().change_scene_to_file("res://level_3.tscn")
+
+
+func _on_level_4_pressed():
+	get_tree().change_scene_to_file("res://level_4.tscn")
+
+
+func _on_button_pressed():
+	get_tree().change_scene_to_file("res://menu.tscn")
